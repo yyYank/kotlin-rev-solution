a369109eebad98029f9a2d8a2f60c13e2e5a2ff0
yy_yank <r.shigetoyo@gmail.com>
2017-02-03 22:21:30 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 
 <html>
     <head>
+        <meta content='summary' name='twitter:card'/>
+        <meta content='http://kotlin-rev-solution.herokuapp.com/images/Kotlin-icon.png' name='twitter:image'/>
+        <meta expr:content='逆引きKotlin' name='twitter:description'/>
         <meta http-equiv="Refresh" content="0; URL=./site">
         <title>逆引きKotlin</title>
     </head>
